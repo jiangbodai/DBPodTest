c0ad49d78b5d811550d9ed23017cc6cbabdcaf8b
jiangbodai <270223447@qq.com>
2021-08-19 15:04:24 +0800

    111111

---
+++ Example/Pods/Target Support Files/DBPodTest/DBPodTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DBPodTest : NSObject
+@end
+@implementation PodsDummy_DBPodTest
+@end
---
+++ Example/Pods/Target Support Files/Pods-DBPodTest_Example/Pods-DBPodTest_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DBPodTest_Example : NSObject
+@end
+@implementation PodsDummy_Pods_DBPodTest_Example
+@end
