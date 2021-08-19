c0d0e2c85f054fd61f602e2d7ff497781e21d7ff
jiangbodai <270223447@qq.com>
2021-08-19 15:12:18 +0800

    22222

---
+++ DBPodTest/Classes/DBPodTestController.h
@@ -0,0 +1,17 @@
+//
+//  DBPodTestController.h
+//  DBPodTest_Tests
+//
+//  Created by IVT502 on 2021/8/19.
+//  Copyright © 2021 jiangbodai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DBPodTestController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DBPodTest/Classes/DBPodTestController.m
@@ -0,0 +1,32 @@
+//
+//  DBPodTestController.m
+//  DBPodTest_Tests
+//
+//  Created by IVT502 on 2021/8/19.
+//  Copyright © 2021 jiangbodai. All rights reserved.
+//
+
+#import "DBPodTestController.h"
+
+@interface DBPodTestController ()
+
+@end
+
+@implementation DBPodTestController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"%s",__func__);
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
